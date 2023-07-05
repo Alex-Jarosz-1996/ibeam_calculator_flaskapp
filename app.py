8e5f2500a8f95b89a0ad4f008dc45b298e2299d2
alexj <alexj@LAPTOP-7048P6HN>
2023-07-06 06:50:55 +1000

    added dynamic distance generation

--- app.py
+++ app.py
@@ -12,10 +12,6 @@ def home() -> str:
         sigma = request.form['sigma']
         force_applied = request.form['force_applied']
         beam_thick = request.form['beam_thick']
-        force_close = request.form['force_close']
-        force1 = request.form['force1']
-        force2 = request.form['force2']
-        force_far = request.form['force_far']
 
         # Process the form data (you can add your own logic here)
 
---
+++ static/js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    // Handle change event of the dropdown
+    $('#num_distances').on('change', function() {
+        var numFields = parseInt($(this).val());
+        generateFields(numFields);
+    });
+});
+
+function generateFields(num) {
+    var dynamicFields = $('#dynamic_fields');
+    dynamicFields.empty(); // Clear previous fields
+
+    for (var i = 1; i <= num; i++) {
+        var label = $('<label></label>').attr('for', 'distance' + i).text('Distance ' + i + ' (m):');
+        var input = $('<input></input>').attr('type', 'text').attr('id', 'distance' + i).attr('name', 'distance' + i).attr('required', true);
+        dynamicFields.append(label).append(input).append('<br><br>');
+    }
+}
