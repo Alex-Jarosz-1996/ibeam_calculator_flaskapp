377226f0a9c2e88bc7961fa9b237f30afe762c6d
alexj <alexj@LAPTOP-7048P6HN>
2023-07-12 07:43:54 +1000

    added git ignore file. Configured app to return calculation after receiving input

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # Import the necessary modules
 from flask import Flask, render_template, request
+from ibeam_calc import ibeam
 
 # Create an instance of Flask
 app: Flask = Flask(__name__)
@@ -9,23 +10,29 @@ app: Flask = Flask(__name__)
 def home() -> str: 
     if request.method == 'POST':
         # Get the form data
-        sigma = request.form['sigma']
-        force_applied = request.form['force_applied']
-        beam_thick = request.form['beam_thick']
+        sigma = float(request.form['sigma'])
+        force_applied = float(request.form['force_applied'])
+        beam_thick = float(request.form['beam_thick'])
         num_distances = int(request.form['num_distances'])
-
-        distances = []
-        for i in range(1, num_distances+1):
-            distance = request.form.get(f"distance{i}")
-            if distance:
-                distances.append(distance)
-
-        # Process the form data (you can add your own logic here)
+        distances = [float(request.form[f'distance{i}']) \
+                     for i in range(1, num_distances+1)]
+        
+        # using the ibeam() function to create the height, width
+        # relationships
+        ibeam_dict = ibeam(sigma, 
+                           force_applied, 
+                           beam_thick, 
+                           distances)
 
         # Render the same template again
-        return render_template('distance.html')
+        return render_template('distance.html', 
+                               sigma=sigma,
+                               force_applied=force_applied,
+                               beam_thick=beam_thick,
+                               results=ibeam_dict)
         
     else:
+        # rendering form template
         return render_template('base.html')
 
 # Run the app
